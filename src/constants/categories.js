a3d7d09437a47d80482a8aa3e522b93679624f29
ilhamirvansyah21 <ilhamirvansyah03@gmail.com>
2021-09-17 09:31:48 +0700

    done

---
+++ src/constants/categories.js
@@ -0,0 +1,30 @@
+export const CATEGORIES = [
+  {
+    name: "Apple",
+    slug: "apple",
+  },
+  {
+    name: "Footballs",
+    slug: "football",
+  },
+  {
+    name: "Netflix",
+    slug: "netflix",
+  },
+  {
+    name: "Games",
+    slug: "games",
+  },
+  {
+    name: "Fifa",
+    slug: "fifa",
+  },
+  {
+    name: "Indonesia",
+    slug: "indonesia",
+  },
+  {
+    name: "Nintendo",
+    slug: "nintendo",
+  },
+];
